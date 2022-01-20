60eea030488c8719cc0e71df05f9a6119361775c
Folen0909 <davidturkalj@gmail.com>
2022-01-20 04:21:20 +0100

    Basic graph for testing.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {BillService} from '../_services/bill.service';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +7,58 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  types = ['Struja', 'Voda', 'Plin', 'Pričuva', 'Odvoz smeća', 'Komunalac', 'HRT', 'Telekomunikacije'];
+  typeValue = ['power', 'water', 'gas', 'reservation', 'trash', 'communal', 'hrt', 'telecom'];
+  options: any;
+  selectedType = 'gas';
 
-  constructor(private router: Router) { }
+  constructor(private billService: BillService) { }
 
   ngOnInit(): void {
-    console.log('HomeComponent INIT');
+    this.refresh();
   }
 
+  refresh() {
+    const xAxisData = [];
+    const cost = [];
+    const counter = [];
+    this.billService.getAll(this.selectedType).subscribe((data) => {
+      data.forEach((bill) => {
+        xAxisData.push(bill.payday);
+        cost.push(bill.cost);
+        counter.push(bill.counter);
+      });
+      this.options = {
+        legend: {
+          data: ['Cijena', 'Potrošnja'],
+          align: 'left',
+        },
+        tooltip: {},
+        xAxis: {
+          data: xAxisData,
+          silent: false,
+          splitLine: {
+            show: false,
+          },
+        },
+        yAxis: {},
+        series: [
+          {
+            name: 'Cijena',
+            type: 'bar',
+            data: cost,
+            animationDelay: (idx) => idx * 10,
+          },
+          {
+            name: 'Potrošnja',
+            type: 'bar',
+            data: counter,
+            animationDelay: (idx) => idx * 10 + 100,
+          },
+        ],
+        animationEasing: 'elasticOut',
+        animationDelayUpdate: (idx) => idx * 5,
+      };
+    });
+  }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,19 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
+import {NgxEchartsModule} from 'ngx-echarts';
+import {MatSelectModule} from "@angular/material/select";
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule]
+  imports: [
+    CommonModule,
+    SharedModule,
+    HomeRoutingModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts')
+    }),
+    MatSelectModule
+  ]
 })
 export class HomeModule {}
